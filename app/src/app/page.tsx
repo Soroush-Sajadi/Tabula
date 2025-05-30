bc8431a628c027c897b868c1ef8c38a1d82a50e2
Soroush Sajadi <soroushsajadi@Soroushs-MacBook-Pro.local>
2025-05-30 17:42:36 +0200

    Europe map is added

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import EuropeMap from "./EuropeMap";
 
 export default function Home() {
-  return <div> WELCOME TO THE TABULA WORLD!</div>;
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Europe Political Map</h1>
+      <EuropeMap />
+    </main>
+  );
 }
